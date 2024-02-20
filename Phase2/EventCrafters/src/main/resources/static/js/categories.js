e8c5d46479634335b01c2f2d14a96802f792e0cf
AlvaroS3rrano <a.serranor.2021@alumnos.urjc.es>
2024-02-20 19:10:14 +0100

    Categories.js fixed

--- Phase2/EventCrafters/src/main/resources/static/js/categories.js
+++ Phase2/EventCrafters/src/main/resources/static/js/categories.js
@@ -1,14 +1,69 @@
+const windowBackground = document.getElementById('window-background'),
+    windowContainer = document.getElementById('window-container'),
+    closeButton = document.getElementById('close-button'),
+    tagColorin = document.getElementById('tag-color-in'),
+    tagNamein = document.getElementById('tag-name-in');
+
 let currentpage = 0;
 
+// to trasition to rgb from hex
+function rgbToHex(r,g,b){
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+const newButton = (e) => {
+    windowBackground.style.display = 'flex'
+    let parent = e.target.parentElement;
+    if (parent.getAttribute('name') == 'tag'){
+        let nameholder = parent.getElementsByTagName('span')
+        tagNamein.value = nameholder[0].textContent
+        let color = window.getComputedStyle(nameholder[0]).getPropertyValue('background-color')
+        let rgb = color.split('(')[1].split(')')[0].split(',').map(function(num) {
+            return parseInt(num.trim());// para pasar los string a integer
+        });
+        tagColorin.value = rgbToHex(rgb[0],rgb[1],rgb[2]);
+    }
+}
+
+// makes it so that all elements with id=open-button do the same function when clicked
+document.getElementById('categories-container').addEventListener('click', (e) => {
+    if (e.target && e.target.matches('#open-button')) {
+        newButton(e)
+    }
+})
+
+//to close the tag popup
+const closeWindow = () => {
+    windowContainer.classList.add('close')// closing animation
+
+    // so that it doesn´t set the values to null until the transition has finished
+    setTimeout(() => {tagColorin.value = null;tagNamein.value = null;}, 500)
+
+    setTimeout(() => {
+        windowContainer.classList.remove('close')
+        windowBackground.style.display = 'none'
+    }, 1000);
+}
+
+// if you click the x simbol the popup closes
+closeButton.addEventListener('click', () => closeWindow())
+
+// if you click outside the popup it closes
+window.addEventListener('click', e => e.target == windowBackground && closeWindow())
+
+tagColorin.addEventListener('input', () => colorTrial.style.background = tagColorin.value)
+
+// AJAX (fetch)
 document.getElementById('load-more').addEventListener('click', function() {
     currentpage++;
-    cargarProductos(currentpage);
+    loadCategories(currentpage);
 });
 
-function cargarProductos(page) {
+function loadCategories(page) {
     fetch(`/categories?page=${page}`)
         .then(response => response.text())
         .then(html => {
             document.getElementById('categories-container').insertAdjacentHTML('beforeend', html);
         });
+
 }
--- Phase2/EventCrafters/src/main/resources/static/js/perfil.js
+++ Phase2/EventCrafters/src/main/resources/static/js/perfil.js
@@ -1,53 +1,4 @@
-const windowBackground = document.getElementById('window-background'),
-    windowContainer = document.getElementById('window-container'),
-    openButtons = document.getElementsByName('open-button'),
-    closeButton = document.getElementById('close-button'),
-    colorTrial = document.getElementById('color-trial'),
-    tagColorin = document.getElementById('tag-color-in'),
-    tagNamein = document.getElementById('tag-name-in'),
-    deleteAccountBtn = document.getElementById('delete-account-btn');
-
-
-// para pasar de rgb a hex
-function rgbToHex(r,g,b){ 
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-
-// openButtons es un array con todos los elementos con el nombre de open-button
-for (var i = 0; i < openButtons.length; i++) {
-    openButtons[i].addEventListener('click', (e) => {
-        windowBackground.style.display = 'flex'
-        let parent = e.target.parentElement;
-        if (parent.getAttribute('name') == 'tag'){
-            let nameholder = parent.getElementsByTagName('span')
-            tagNamein.value = nameholder[0].textContent
-            let color = window.getComputedStyle(nameholder[0]).getPropertyValue('background-color')
-            let rgb = color.split('(')[1].split(')')[0].split(',').map(function(num) {
-                return parseInt(num.trim());// para pasar los string a integer
-              });
-            tagColorin.value = rgbToHex(rgb[0],rgb[1],rgb[2]);
-        } 
-    })
-}
-
-//para cerrar el popup de tags
-const closeWindow = () => {
-    windowContainer.classList.add('close') // animación de cerrado
-    setTimeout(() => {
-        windowContainer.classList.remove('close')
-        windowBackground.style.display = 'none'
-    }, 1000);
-}
-
-// so pulsas en la x se cierra la ventana
-closeButton.addEventListener('click', () => closeWindow())
-
-// para qui si pulsas fuera de la ventana se salga de esta
-window.addEventListener('click', e => e.target == windowBackground && closeWindow())
-
-tagColorin.addEventListener('input', () => colorTrial.style.background = tagColorin.value)
-
+const deleteAccountBtn = document.getElementById('delete-account-btn');
 
 const askForConfirmation = () => {
     var result = confirm("¿Estás seguro de que deseas continuar?");
@@ -59,6 +10,5 @@ const askForConfirmation = () => {
         alert("Has cancelado la acción.");
     }
 }
-
 deleteAccountBtn.addEventListener('click', () => askForConfirmation())
 
