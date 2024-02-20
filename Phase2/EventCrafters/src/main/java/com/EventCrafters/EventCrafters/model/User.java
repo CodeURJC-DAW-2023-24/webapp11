180a85349255cad67f4f670c75363251320b950b
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2024-02-20 09:54:49 +0100

    Fixed login also added DB debugging
    Title. Also changed DB initialization to delete previous tables and create them again.
    
    Co-Authored-By: Ángel Marqués García <117300926+AngelMarquesGarcia@users.noreply.github.com>

--- Phase2/EventCrafters/src/main/java/com/EventCrafters/EventCrafters/model/User.java
+++ Phase2/EventCrafters/src/main/java/com/EventCrafters/EventCrafters/model/User.java
@@ -39,7 +39,7 @@ public class User {
 	private String encodedPassword;
 
 
-	@ElementCollection(fetch = FetchType.LAZY)
+	@ElementCollection(fetch = FetchType.EAGER)
 	private List<String> roles;
 
 	public User() {
