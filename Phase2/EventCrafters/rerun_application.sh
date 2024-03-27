2b02267c750ff0f597d344c148f668f3afeb4b1a
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2024-03-27 16:52:28 +0100

    Update rerun_application.sh

--- Phase2/EventCrafters/rerun_application.sh
+++ Phase2/EventCrafters/rerun_application.sh
@@ -1,10 +1,10 @@
 docker compose down
-rm -r mysql
+sudo rm -r mysql
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker volume prune
 cd ~
-rm -r webapp11
+sudo rm -r webapp11
 git clone https://github.com/CodeURJC-DAW-2023-24/webapp11.git
 cd webapp11/Phase2/EventCrafters
 docker build -t luciadominguezrodrigo/eventcrafters .
