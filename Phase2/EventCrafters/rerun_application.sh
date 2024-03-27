a4709c0d65f378cf7f9bbc0998abc0b874bdb271
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2024-03-27 16:47:51 +0100

    Added docker commands automated file for virtual machine instancing
    Simplified building and execution of the webapp by using command ./rerun_application.sh on project directory.

---
+++ Phase2/EventCrafters/rerun_application.sh
@@ -0,0 +1,11 @@
+docker compose down
+rm -r mysql
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+docker volume prune
+cd ~
+rm -r webapp11
+git clone https://github.com/CodeURJC-DAW-2023-24/webapp11.git
+cd webapp11/Phase2/EventCrafters
+docker build -t luciadominguezrodrigo/eventcrafters .
+docker compose up
